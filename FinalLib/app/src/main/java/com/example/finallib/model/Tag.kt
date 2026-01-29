92fb7b56adcff44e6595ee65c762f9c4c08c0580
duyngo116 <90699126+duyngo116@users.noreply.github.com>
2026-01-29 13:26:24 +0700

    Delete FinalLib directory

--- FinalLib/app/src/main/java/com/example/finallib/model/Tag.kt
+++
@@ -1,7 +0,0 @@
-package com.example.finallib.model
-
-data class Tag(
-    val id: String = "",  // Tên tag (vd: "Fiction")
-    val label: String = "",  // Tên tag (vd: "Fiction")
-    val value: String = ""  // id lowercase (vd: "fiction")
-)
